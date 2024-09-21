4820340aefb6c61076e31909c04f68b4741a7983
Бакиев Айрат Иршатович <AIBakiev@sberbank.ru>
2024-09-21 22:41:23 +0300

    main: feature create repositories

---
+++ Backend.Project_2-1/backend_2/src/main/java/edu/backend/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package edu.backend.repositories;
+
+import edu.backend.entities.AddressEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
+}
