fc45359d57b789b0359191b9aa2b6b122b20ba0a
Бакиев Айрат Иршатович <AIBakiev@sberbank.ru>
2024-10-09 13:18:18 +0300

    delete backend 2

--- Backend.Project_2-1/backend_2/src/main/java/edu/backend/repositories/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.backend.repositories;
-
-import edu.backend.entities.AddressEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
-}
